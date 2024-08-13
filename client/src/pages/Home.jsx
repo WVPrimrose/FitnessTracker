9209af2c66246d7d68325e90f893a11fdeb0f75a
Katie Buchholz <kzbindy@hotmail.com>
2024-08-12 20:48:46 -0400

    got the very barebones of the home page; it's not much, but don't know if we'll be using it

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom'
+import Navbar from './Navbar'
+import Login from './Login'
+import Signup from './Signup'
+
+
+const HomePage = () => {
+
+    return(
+        <>
+        <h1>Fitness Tracker</h1>
+        <h2>We love to workout to reach our goals</h2>
+        
+        
+        </>
+    )
+
+
+}
+
+export default HomePage

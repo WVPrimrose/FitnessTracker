ecb8cfb956b619813d1fc146ef65e6ecf1710727
Katie Buchholz <kzbindy@hotmail.com>
2024-08-12 20:54:10 -0400

    very barebones with the dashboard; it's hardly anything, but it's something

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,10 @@
+return (
+    <>
+    <h1>My dashboard</h1>
+    <h2>My profile</h2>
+    <h2>My workouts</h2>
+    <h2>My goals</h2>
+    <button>Create a new goal</button>
+    <button></button>
+    </>
+)

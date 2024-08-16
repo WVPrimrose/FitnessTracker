b4a539f858980db7de296111a9bdd558c0d27f54
SlayerK2024 <157855730+SlayerK2024@users.noreply.github.com>
2024-08-15 20:49:23 -0400

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,38 +1,43 @@
-import { useQuery } from '@apollo/client';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import myImage1 from '../assets/images/Logo.png';
+import myImage2 from '../assets/images/gettyimages-1256375779.jpg';
+import myImage3 from '../assets/images/gym.jpg';
+import myImage4 from '../assets/images/outdoorexercise.jpg';
+import myImage5 from '../assets/images/water.jpg';
 
-import  from '../components/ProfileList';
-import  from '../components/ProfileForm';
-
-import {  } from '../utils/';
-
-const Home = () => {
-  const { loading, data } = useQuery();
-
-  const profiles = data?.profiles || [];
-
+const HomePage = () => {
   return (
-    <main>
-      <div className="flex-row justify-center">
-        <div
-          className="col-12 col-md-10 mb-3 p-3"
-          style={{ border: '1px dotted #1a1a1a' }}
-        >
-          < />
+    <div>
+      <h1>Welcome to Your Fitness Tracker</h1>
+      <h2>FITNESS FIRST</h2>
+      <Carousel>
+      <div>
+          <img src={myImage1} alt="Logo" />
         </div>
-
-        <div className="col-12 col-md-10 my-3">
-          {loading ? (
-            <div>Loading...</div>
-          ) : (
-            <
-              profiles={profiles}
-              title="Let's check on your progress"
-            />
-          )}
+        <div>
+          <img src={myImage2} alt="Plank" />
         </div>
-      </div>
-    </main>
+        <div>
+          <img src={myImage3} alt="Lifting" />
+        </div>
+        <div>
+          <img src={myImage4} alt="Running" />
+        </div>
+        <div>
+          <img src={myImage5} alt="Drinking water" />
+        </div>
+      </Carousel>
+      <Link to="/login">
+        <button>Login</button>
+      </Link>
+      <Link to="/Signup">
+        <button>Sign Up</button>
+      </Link>
+    </div>
   );
 };
 
-export default Home;
+export default HomePage;

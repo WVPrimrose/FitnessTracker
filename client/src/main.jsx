497fe29900fd01ab3f34e1e07bc4af76fc74f369
Blitzbomber87 <elias.efly000@gmail.com>
2024-08-15 18:48:11 -0700

    Render Homepage

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,17 +3,18 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 // import LoginForm from '../src/pages/Login';
 import App from './App.jsx'
+import Home from './pages/Home'
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
     errorElement: <h1 className='display-2'>Wrong page!</h1>,
-    /* children: [
+    children: [
       {
        index: true,
-        element: <Homepage />,
+        element: <Home />,
       },
-    ], */
+    ], 
   },
 ]);
 

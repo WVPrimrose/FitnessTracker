7435b7f2ef4d3e4dc976b50384700a41a197fead
Katie Buchholz <kzbindy@hotmail.com>
2024-08-15 20:19:46 -0400

    have a rough draft of the dashboard page; not the prettiest, but hopefully, it'll be worked on more

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,1 +1,38 @@
+// importing packages
+import { useState } from "react";
+import { useQuery } from '@apollo/client';
+import { useParams } from 'react-router-dom'
 
+const Dashboard = () => {
+    const { profileId } = useParams();
+    const { loading, data } = useQuery(singleProfile, {
+        variables: { profileId: profileId },
+    });
+
+    const profile = data?.profile || {};
+
+    if(loading) {
+        return <div>
+            <h1>LOADING...</h1>
+            <p>I know.  Waiting is hard.</p>
+        </div>;
+    }
+
+    return (
+        <>
+        <h1>My Dashboard</h1>
+        <div>
+            <h2>My Current Goals</h2>
+        </div>
+        <div>
+            <h2>My Current Workout Logs</h2>
+        </div>
+        <div>
+            <button><h2>Create a New Goal</h2></button>
+        </div>
+        <div>
+            <button><h2>Create a New Workout Log</h2></button>
+        </div>
+        </>
+    )
+}

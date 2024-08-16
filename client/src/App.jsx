7eb59d002e90a79bad87c1094575a69700651902
Blitzbomber87 <elias.efly000@gmail.com>
2024-08-15 18:23:57 -0700

    Fixed Login link and Login Form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Navbar from './components/Navbar';
 
